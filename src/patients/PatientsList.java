4dc5c80c4977602b01adb7b00f55094daf5b39aa
Elí Alvarado <elalvaradoob@est.utn.ac.cr>
2026-08-13 15:34:33 -0600

    Cambios en PatientsList

--- src/patients/PatientsList.java
+++ src/patients/PatientsList.java
@@ -3,11 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package patients;
-
+import clinic.KeyDynamicsLists;
+import java.util.HashMap;
+import java.util.l.Iterator;
 /**
  *
  * @author emalv
  */
-public class PatientsList {
+public class PatientsList implements KeyDynamicList<Patient,String>{
+    private HashMap<String,Patient> patients;
+    
+    
     
 }
