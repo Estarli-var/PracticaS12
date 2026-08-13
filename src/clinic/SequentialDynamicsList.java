3663ba08663631c7b68ee34ce879c1b1fa4d8051
Wesley <wruizm@est.utn.ac.cr>
2026-08-13 14:16:24 -0600

    Se agrega clase de Sequential
    se agrega metodos

---
+++ src/clinic/SequentialDynamicsList.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package clinic;
+
+/**
+ *
+ * @author Student
+ */
+public interface SequentialDynamicsList<T,K> extends DynamicsList<T,K>{
+    public T get();
+    public boolean remove();
+    
+}
